2000010fd437cd1f21310ddb37286dbcd234b41c
nmlynch94 <nmlynch94@gmail.com>
2023-07-22 13:17:43 -0400

    Installs and runs the jagex launcher

--- jagex-run.sh
+++ jagex-run.sh
@@ -7,22 +7,27 @@ jagex_launcher_url="https://cdn.jagex.com/Jagex%20Launcher%20Installer.exe"
 
 first_run="false"
 
-if [ ! -f "$XDG_DATA_HOME/jagex-launcher/JagexLauncher.exe" ]; then
+if [ ! -f "$XDG_DATA_HOME/jagex-launcher/prefix/drive_c/Program Files (x86)/Jagex Launcher/JagexLauncher.exe" ]; then
     echo "First Run!"
     first_run="true"
 fi
 
 if [ "$first_run" == "true" ]; then
+    # Create directory for launcher data
     mkdir -p "$XDG_DATA_HOME"/jagex-launcher
     cd jagex-launcher
-    echo "First time installation!"
-    echo "Download Jagex Launcher Installer..."
+    # Grab lutris-ge-wine
+    wget -O wine-ge.tar.xz https://github.com/GloriousEggroll/wine-ge-custom/releases/download/GE-Proton8-12/wine-lutris-GE-Proton8-12-x86_64.tar.xz
+    tar -xf wine-ge.tar.xz
+    mv lutris-GE-Proton8-12-x86_64 wine
+    
+    # Download the jagex launcher installer
     wget -O jagex-launcher-installer.exe "$jagex_launcher_url"
-    cp -r /app/opt/jagex-launcher/* "$XDG_DATA_HOME/jagex-launcher"
-    mkdir -p "$XDG_DATA_HOME"/jagex-launcher/prefix
-    WINEPREFIX="$XDG_DATA_HOME/jagex-launcher/prefix" winetricks --unattended corefonts
-    WINEPREFIX="$XDG_DATA_HOME/jagex-launcher/prefix" WINEDLLOVERRIDES="jscript.dll=n" wine "jagex-launcher-installer.exe"
-fi
-# cd "$XDG_DATA_HOME"
-# WINEPREFIX="$XDG_DATA_HOME/prefix" wine JagexLauncher.exe
 
+    # Create wine prefix directory and launch the installer
+    mkdir -p "$XDG_DATA_HOME"/jagex-launcher/prefix
+    WINEPREFIX="$XDG_DATA_HOME/jagex-launcher/prefix" WINEDLLOVERRIDES="jscript.dll=n" ./wine/bin/wine "jagex-launcher-installer.exe"
+else
+    cd jagex-launcher
+fi
+WINEPREFIX="$XDG_DATA_HOME/jagex-launcher/prefix" ./wine/bin/wine "./prefix/drive_c/Program Files (x86)/Jagex Launcher/JagexLauncher.exe"
